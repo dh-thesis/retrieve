bf5b2f9e517a737d078261bea2efc69877ff8ab0
herreio <mail@herre.io>
2019-08-10 18:54:13 +0200

    exclude crawling/mapping

--- src/__main__.py
+++ src/__main__.py
@@ -40,19 +40,6 @@ from .pure_ous import *
 
 from .pure_pub import *
 
-# /////////////////// #
-# /// INSTITUTES /// #
-# ///////////////// #
-
-from .crawl_mpis_eng import *
-from .crawl_mpis_deu import *
-
-from .map_mpis_eng import *
-from .map_mpis_deu import *
-
-from .map_mpis import *
-from .map_post import *
-
 # ///////////// #
 
 print("finished retrieval after %s sec!" % round(time.time() - start_time, 2))
